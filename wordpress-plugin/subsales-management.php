fdc2d20beb67c968da177a5c6a5cb7e0834b578e
jimmarks <jim@marksfamilytree.com>
2026-01-07 01:42:44 +0000

    Fix syntax error: remove stray </script> tag at end of file

--- wordpress-plugin/subsales-management.php
+++ wordpress-plugin/subsales-management.php
@@ -3,7 +3,7 @@
  * Plugin Name: Subsales Management
  * Plugin URI: https://github.com/jimmarks/Southington-BKMB-Subsales
  * Description: A comprehensive order management system for mobile app synchronization with WordPress backend. Includes multi-team management, Google Maps integration, and professional admin interface. ⚠️ WARNING: By default, deleting this plugin will permanently remove ALL data. Configure deletion settings in BKMB Subsales → Settings.
- * Version: 2.2.1.77
+ * Version: 2.2.1.78
  * Author: Jim Marks
  * Author URI: https://github.com/jimmarks
  * Requires at least: 5.0
@@ -11629,9 +11629,5 @@ function subsales_ajax_get_campaign_signups() {
     }
     
     wp_send_json_success( array( 'html' => $html ) );
-}
-
-    </script>
-    <?php
 }
 
